b688a6b03d0f4086f536fa0624b842134963ef9e
史凯迪 <caydyn@icloud.com>
2015-08-12 17:38:25 +0800

    第一个Demo

---
+++ PopViewWithButton/popBarView.swift
@@ -0,0 +1,121 @@
+//
+//  popBarView.swift
+//
+//  Created by 史凯迪 on 15/8/12.
+//  Copyright © 2015年 msy. All rights reserved.
+//
+
+import UIKit
+
+class popBarView: UIView {
+    /* 展开后的大小 */
+    private var viewFrameBeforePop: CGRect!
+    private var viewFrameAfterPop: CGRect!
+    
+    /* 默认按钮 (赞和分享)*/
+    private var useDefaultButton: Bool = true
+    private var greatButton: UIButton!
+    private var shareButton: UIButton!
+    internal var greatBtnAction: (() -> Void)!
+    internal var shareBtnAction: (() -> Void)!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.viewFrameBeforePop = frame
+        /* 圆角边框 */
+        self.layer.masksToBounds = true
+        self.layer.cornerRadius = 5.0
+        
+        /* 默认仅仅添加两个按钮，赞和分享 */
+        self.addDefaultButton()
+    }
+    
+    /* 传入自定义的Buttons，仅仅处理添加 */
+    convenience init(frame: CGRect, afterPopFrame: CGRect, buttons: [UIButton]?) {
+        self.init(frame: frame)
+        self.viewFrameAfterPop = afterPopFrame
+        
+        self.removeDefaultButton()
+        
+        if let addButtons = buttons {
+            /* 计算每个Button的布局(平均分布) */
+            let buttonWidth: CGFloat = (afterPopFrame.width / CGFloat(addButtons.count))
+            let buttonHeight: CGFloat = afterPopFrame.height
+            /* 添加自定义的按钮 */
+            var buttonXOffset: CGFloat = 0
+            for button in addButtons {
+                button.frame = CGRect(x: buttonXOffset, y: 0,
+                    width: buttonWidth, height: buttonHeight)
+                self.addSubview(button)
+                buttonXOffset += buttonWidth
+            }
+        }
+    }
+    
+    private func addDefaultButton() {
+        let defaultButtonAction: Selector = Selector("defaultBtnAction:")
+        
+        let greateButtonFrame: CGRect = CGRect(x: 0, y: 0,
+            width: 50, height: 30)
+        self.greatButton = self.createButton(greateButtonFrame,
+            btnAction: defaultButtonAction)
+        self.greatButton.tag = 0
+        
+        let shareButtonFrame: CGRect = CGRect(x: 50, y: 0,
+            width: 50, height: 30)
+        self.shareButton = self.createButton(shareButtonFrame,
+            btnAction: defaultButtonAction)
+        self.shareButton.tag = 1
+        
+        self.addSubview(greatButton)
+        self.addSubview(shareButton)
+    }
+    
+    private func removeDefaultButton() {
+        self.greatButton.removeFromSuperview()
+        self.shareButton.removeFromSuperview()
+        
+        self.greatButton = nil
+        self.shareButton = nil
+    }
+    
+    internal func setDefaultButtonSetTitle() {
+        if let _ = self.greatButton {
+            self.greatButton.setTitle("赞", forState: .Normal)
+        }
+        if let _ = self.shareButton {
+            self.shareButton.setTitle("分享", forState: .Normal)
+        }
+    }
+    
+    private func createButton(frame: CGRect, btnAction: Selector) -> UIButton {
+        let button: UIButton = UIButton(type: .Custom)
+        button.frame = frame
+        button.titleLabel?.font = UIFont.boldSystemFontOfSize(12)
+        button.addTarget(self, action: btnAction, forControlEvents: .TouchUpInside)
+        return button
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    enum defaultBtnType: Int {
+        case greatBtn = 0;
+        case shareBtn = 1;
+    }
+    
+    func defaultBtnAction(sender: UIButton) {
+        let btnType: defaultBtnType = defaultBtnType(rawValue: sender.tag)!
+        switch btnType {
+        case .greatBtn:
+            if let greatAction = self.greatBtnAction {
+                greatAction()
+            }
+        case .shareBtn:
+            if let shareAction = self.shareBtnAction {
+                shareAction()
+            }
+        }
+    }
+}
---
+++ PopViewWithButton/popBarViewButton.swift
@@ -0,0 +1,151 @@
+//
+//  popBarViewButton.swift
+//
+//  Created by 史凯迪 on 15/8/12.
+//  Copyright © 2015年 msy. All rights reserved.
+//
+
+import UIKit
+
+class popBarViewButton: UIButton {
+    
+    /* 弹出的方向 */
+    enum popViewDirection: Int {
+        case popToLeft = 1;
+        case popToRight = 2;
+    }
+    
+    /* 弹出的大小设定, 更具需要修改 */
+    let popBarViewWidth: CGFloat = 100
+    let popBarViewHeight: CGFloat = 30
+    let popbarViewDistanceWithButton: CGFloat = 5
+    let popDirection: popViewDirection = .popToLeft
+    
+    /* 计算弹出的Frame */
+    private var popBarViewOriginalFrame: CGRect!
+    private var popBarViewFrame: CGRect!
+    
+    /* 判断 */
+    private var popBarViewHasInit: Bool = false
+    private var popBarViewHasShow: Bool = false
+    private var xpopBarView: popBarView?
+    private var customPopBarView: Bool = false
+    private var actionAfterPop: (() -> Void)!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.initPopBarView(frame)
+    }
+    
+    convenience init(frame: CGRect, greateAction: (() -> Void),
+        shareAction: (() -> Void)) {
+            self.init(frame: frame)
+            self.setDefaultAction(greateAction, shareAction: shareAction)
+    }
+    
+    convenience init(frame: CGRect, afterPopFrame: CGRect,
+        popBarViewBtns: [UIButton]?, actionAfterPop: (() -> Void)?) {
+            self.init(frame: frame)
+            
+            if let buttons = popBarViewBtns {
+                self.customPopBarView = true
+                self.actionAfterPop = actionAfterPop
+                self.xpopBarView = popBarView(frame: self.popBarViewOriginalFrame,
+                    afterPopFrame: self.popBarViewFrame, buttons: buttons)
+            }
+    }
+    
+    private func initPopBarView(buttonFrame: CGRect) {
+        
+        var popBarViewOriginalX: CGFloat = 0
+        var popBarViewX: CGFloat = 0
+        var popBarViewY: CGFloat = 0
+        
+        /* 计算弹出视图的位置 */
+        if buttonFrame.height > self.popBarViewHeight{
+           popBarViewY = buttonFrame.origin.y + ((buttonFrame.height - self.popBarViewHeight) / 2)
+        } else {
+            popBarViewY = buttonFrame.origin.y - ((self.popBarViewHeight - buttonFrame.height) / 2)
+        }
+        switch self.popDirection {
+        case .popToLeft:
+            popBarViewOriginalX =
+                buttonFrame.origin.x - popbarViewDistanceWithButton
+            popBarViewX =
+                buttonFrame.origin.x - (popbarViewDistanceWithButton + self.popBarViewWidth)
+        case .popToRight:
+            popBarViewOriginalX =
+                buttonFrame.origin.x + popbarViewDistanceWithButton
+            popBarViewX =
+                buttonFrame.origin.x + (popbarViewDistanceWithButton + self.popBarViewWidth)
+            break
+        }
+        
+        /* 初始化的位置 */
+        self.popBarViewOriginalFrame =
+            CGRect(x: popBarViewOriginalX, y: popBarViewY,
+                width: 0, height: popBarViewHeight)
+        /* 弹出后的位置 */
+        self.popBarViewFrame =
+            CGRect(x: popBarViewX, y: popBarViewY,
+                width: self.popBarViewWidth, height: self.popBarViewHeight)
+        /* 默认初始化 */
+        self.xpopBarView = popBarView(frame: self.popBarViewOriginalFrame)
+        
+        self.popBarViewHasInit = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    private func showPopBarView() {
+        self.popBarViewHasShow = true
+        
+        self.xpopBarView!.backgroundColor = UIColor.grayColor()
+        self.superview?.addSubview(self.xpopBarView!)
+        
+        UIView.animateWithDuration(0.3, delay: 0.0,
+            options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
+                self.xpopBarView!.frame = self.popBarViewFrame
+            }) { (finish) -> Void in
+                if self.customPopBarView {
+                    self.actionAfterPop()
+                } else {
+                    self.xpopBarView?.setDefaultButtonSetTitle()
+                }
+        }
+    }
+    
+    private func dismissPopBarView() {
+        self.popBarViewHasShow = false
+        
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+            self.xpopBarView!.frame = self.popBarViewOriginalFrame
+            }, completion: { (finish) -> Void in
+                self.xpopBarView?.removeFromSuperview()
+        })
+    }
+    
+    internal func setDefaultAction(greatAction: (() -> Void)?,
+        shareAction: (() -> Void)?) {
+            self.xpopBarView?.greatBtnAction = greatAction
+            self.xpopBarView?.shareBtnAction = shareAction
+    }
+    
+    internal func togglePopView(buttonFrame: CGRect) {
+        if !self.popBarViewHasInit {
+            self.initPopBarView(buttonFrame)
+        }
+        
+        if self.popBarViewHasShow {
+            self.dismissPopBarView()
+        } else {
+            self.showPopBarView()
+        }
+    }
+    
+    deinit {
+        self.xpopBarView = nil
+    }
+}
